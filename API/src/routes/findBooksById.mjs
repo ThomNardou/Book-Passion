2ae06f10c3382580a47a683d066c65b47ed79a9a
joaberch <122774888+joaberch@users.noreply.github.com>
2024-03-02 09:25:40 +0100

    Simplify the code

--- API/src/routes/findBooksById.mjs
+++ API/src/routes/findBooksById.mjs
@@ -1,12 +1,12 @@
 import express from "express";
 import { books } from "../db/mock-books.mjs";
-import { success } from "./helper.mjs";
+import { success, getBook } from "./helper.mjs";
 
 const idBooksRooter = express();
 
 idBooksRooter.get("/:id", (req, res) => {
   const bookId = req.params.id;
-  const book = books.find((book) => book.id == bookId)
+  const book = getBook(bookId)
   const message = `The book with id ${bookId} has been retrieved.`;
   res.json(success(message, book));
 });
