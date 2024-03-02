e0a4239545cfbcbd73a5f73e57eb189ad4902280
joaberch <122774888+joaberch@users.noreply.github.com>
2024-03-02 11:40:59 +0100

    Update swagger documentation

--- API/src/routes/createBooks.mjs
+++ API/src/routes/createBooks.mjs
@@ -9,34 +9,14 @@ const createBooksRouter = express();
  * @swagger
  * /api/books/:
  *   get:
- *     tags: [Products]
+ *     tags: [Books]
  *     security:
  *       - bearerAuth: []
- *     summary: Retrieve all products.
- *     description: Retrieve all products. Can be used to populate a select HTML tag.
+ *     summary: Retrieve all books.
+ *     description: Retrieve all books.
  *     responses:
  *       200:
- *         description: All books.
- *         content:
- *         application/json:
- *           schema:
- *             type: object
- *             properties:
- *               data:
- *                 type: object
- *                 properties:
- *                   id:
- *                     type: integer
- *                     description: The product ID.
- *                     example: 1
- *                   name:
- *                     type: string
- *                     description: The product's name.
- *                     example: Big Mac
- *                   price:
- *                     type: number
- *                     description: The product's price.
- *                     example: 5.99
+ *         description: Retrieve all books.
  */
 createBooksRouter.post("/", (req, res) => {
     const id = getUniqueId(books);
