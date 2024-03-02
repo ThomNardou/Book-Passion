6fac6ae0e9af6cbc34ffc61ad0a8408615aab3c5
joaberch <122774888+joaberch@users.noreply.github.com>
2024-03-02 08:51:33 +0100

    Add function to helper

--- API/src/routes/helper.mjs
+++ API/src/routes/helper.mjs
@@ -7,10 +7,41 @@ const success = (message, data) => {
   };
 };
 
+/**
+* Generates and returns the next book id
+* @param {*} books
+*/
 const getUniqueId = (books) => {
   const booksIds = books.map((book) => book.id);
   const maxId = booksIds.reduce((a, b) => Math.max(a, b));
   return maxId + 1;
-  };
+};
 
-export { success, getUniqueId };
+/**
+* Retrieves the book whose id is `bookId`.
+* @param {*} bookId
+*/
+const getBook = (bookId) => {
+  return books.find((book) => book.id == bookId);
+};
+
+/**
+* Deletes the book whose id is `bookId`.
+* @param {*} bookId
+*/
+const removeBook = (bookId) => {
+  books = books.filter((book) => book.id != bookId);
+};
+
+/**
+* Updates the book whose id is `bookId`.
+* @param {*} bookId
+* @param {*} updatedbook
+*/
+const updatebook = (bookId, updatedbook) => {
+  books = books.map((book) =>
+    book.id == bookId ? updatedbook : book
+  );
+};
+
+export { success, getUniqueId, getBook, removeBook, updatebook };
