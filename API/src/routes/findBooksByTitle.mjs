bf59e0bc15aefd701bb64f62656125f730cb8d8f
joaberch <122774888+joaberch@users.noreply.github.com>
2024-03-04 10:06:01 +0100

    Fix issue on findBooksByTitle routes, catch error

--- API/src/routes/findBooksByTitle.mjs
+++ API/src/routes/findBooksByTitle.mjs
@@ -19,10 +19,21 @@ const titleBooksRooter = express();
  *         description: Retrieve a book using it's title.
  */
 titleBooksRooter.get("/:title", (req, res) => {
-  Book.findOne({where: {title: 'monster'}}).then((book) => {
-    const message = `The book with title ${req.params.title} has been retrieved.`;
+  const title = req.params.title
+
+  Book.findOne({where: {title: title}}).then((book) => {
+    if (book === null) {
+      const message = "The requested product does not exist. Please try again with another login.";
+      return res.status(404).json({ message });
+    }
+
+    const message = `The book with title ${book.title} has been retrieved.`;
     res.json(success(message, book));
-  });
+  })
+  .catch((error) => {
+    const message = "The product could not be recovered. Please try again shortly.";
+    res.status(500).json({ message, data: error });
+  })
 });
 
 export { titleBooksRooter };
