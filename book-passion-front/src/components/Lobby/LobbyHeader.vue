d4db75ed4616cba5bfc47e9b06e5a27eb1c5f8ab
ReactedTitan880 <thomasnardou@icloud.com>
2024-04-22 10:34:03 +0200

    finish de lobby header

--- book-passion-front/src/components/Lobby/LobbyHeader.vue
+++ book-passion-front/src/components/Lobby/LobbyHeader.vue
@@ -1,13 +1,68 @@
 <template>
-    <div>
-        <h1>gsreghsrhsrhsrhsrrh</h1>
+  <header>
+    <div class="text">
+      <div>
+        <h1>BookPassion</h1>
+        <h2>A passion for reading...</h2>
+      </div>
     </div>
+    <div class="image">
+      <img src="/pile-de-livres.png" alt="" />
+    </div>
+  </header>
 </template>
 <script>
-export default {
-    
-}
+export default {};
 </script>
-<style>
-    
+<style scoped>
+header {
+  height: 100vh;
+  background: linear-gradient(to bottom, rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.7)),
+    url("/bookWallPaper.jpeg");
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-position: center;
+  color: white;
+  display: flex;
+  justify-content: space-between;
+  align-items: end;
+}
+
+h1,
+h2 {
+  font-family: kanit;
+  font-size: 3.5rem;
+  margin: 0;
+}
+
+h2 {
+    font-weight: normal;
+}
+
+.text {
+  width: 50%;
+  border-right: 5px white solid;
+  display: flex;
+  align-items: center;
+}
+
+.image {
+  width: 50%;
+  display: flex;
+  align-items: center;
+}
+
+.image img {
+  width: 30%;
+}
+
+.text,
+.image {
+  height: 200px;
+  margin-bottom: calc(20% - 200px);
+}
+
+.text div, .image img {
+    margin-left: 15%;
+}
 </style>
