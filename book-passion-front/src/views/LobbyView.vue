6c555396d4f1cf17f97756fff512e1749adfea9f
ReactedTitan880 <thomasnardou@icloud.com>
2024-04-22 10:05:28 +0200

    add lobby View

---
+++ book-passion-front/src/views/LobbyView.vue
@@ -0,0 +1,15 @@
+<template>
+    <LobbyHeader />
+</template>
+
+<script setup>
+import LobbyHeader from '../components/Lobby/LobbyHeader.vue'
+</script>
+
+<script>
+
+</script>
+
+<style>
+    
+</style>
