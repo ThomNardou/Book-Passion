4ccbb3ebc131ffc88ff7e43c6acc267ae91ac1b5
joaberch <122774888+joaberch@users.noreply.github.com>
2024-04-23 15:54:34 +0200

    add the oneBook page

--- book-passion-front/src/router/index.js
+++ book-passion-front/src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import LobbyView from '@/views/LobbyView.vue'
+import OneBookView from '@/views/oneBookView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -8,6 +9,11 @@ const router = createRouter({
       path: '/',
       name: 'home',
       component: LobbyView
+    },
+    {
+      path: '/book',
+      name: 'book',
+      component: OneBookView
     }
   ]
 })
